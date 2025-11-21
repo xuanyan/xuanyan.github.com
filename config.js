dd97666260e75da9b9f7e1dd402425e2d0b025b2
xuanyan <xuanyan1983@gmail.com>
2025-11-21 15:38:04 +0800

    test

---
+++ config.js
@@ -0,0 +1,4 @@
+window.__CONFIG__ = {
+  SUPABASE_URL: "https://xizpdugtjrrkbyygjbek.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhpenBkdWd0anJya2J5eWdqYmVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MTgyMzAsImV4cCI6MjA3ODQ5NDIzMH0.tex3poXCgd-ohazw3rT4Y-A4Zg7A177rBuhT7fbbX_Q"
+};
